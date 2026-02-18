e36d9baaa4aa79c96a06015b35d78b616ff7c6ff
Animenosekai <animenosekai.mail@gmail.com>
2026-02-18 23:01:14 +0100

    tests: add includes to main.c

--- tests/main.c
+++ tests/main.c
@@ -1,3 +1,14 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
+
 #include "snow/snow.h"
 
 snow_main();
